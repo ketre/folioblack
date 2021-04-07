de98a15dc8d0d185dc73e215a4b30e9a419b111a
Anna Ketre <anna.ketre@gmail.com>
2021-04-08 00:17:10 +0100

    flexupd

---
+++ script.js
@@ -0,0 +1,23 @@
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     $('.header').fadeIn();
+//   } else {
+//         $('.header').fadeOut();
+// }
+//   prevScrollpos = currentScrollPos;
+// }
+
+
+
+
+// $(document).scroll(function () {
+//     var y = $(this).scrollTop();
+//     if (y > y-1) {
+//         $('.header').fadeOut();
+//     } else {
+//         $('.header').fadeIn();
+//     }
+
+// });
