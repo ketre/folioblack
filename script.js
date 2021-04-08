c1149cf7c0d1e86971f68435e8ed4ac659794198
Anna Ketre <anna.ketre@gmail.com>
2021-04-08 17:05:52 +0100

    script

--- script.js
+++ script.js
@@ -1,23 +1,51 @@
-// var prevScrollpos = window.pageYOffset;
-// window.onscroll = function() {
-//   var currentScrollPos = window.pageYOffset;
-//   if (prevScrollpos > currentScrollPos) {
-//     $('.header').fadeIn();
-//   } else {
-//         $('.header').fadeOut();
-// }
-//   prevScrollpos = currentScrollPos;
-// }
+        window.addEventListener('load', ()=>{
+        resize(); // Resizes the canvas once the window loads
+        document.addEventListener('mousedown', startPainting);
+        document.addEventListener('mouseup', stopPainting);
+        document.addEventListener('mousemove', sketch);
+        window.addEventListener('resize', resize);
+    }); 
+    function resize(){
+        ctx.canvas.width = window.innerWidth;
+        ctx.canvas.height = window.innerHeight;
+      }   
+    const canvas = document.querySelector('#myCanvas');
+    const ctx = canvas.getContext('2d');
+    let coord = {x:0 , y:0}; 
 
+      let pressed = false;
 
+      // update mouse pointer coordinates
+      document.onmousemove = function(e) {
+        coord.x = (window.Event) ? e.pageX : e.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
+        coord.y = (window.Event) ? e.pageY : e.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
+      }
+      function getPosition(event){
+  coord.x = event.clientX - canvas.offsetLeft;
+  coord.y = event.clientY - canvas.offsetTop;
+}
+let paint = false;
+function startPainting(event){
+  paint = true;
+  getPosition(event);
+}
+function stopPainting(){
+  paint = false;
+}
+      canvas.onmousedown = function() {
+        pressed = true;
+      };
 
-
-// $(document).scroll(function () {
-//     var y = $(this).scrollTop();
-//     if (y > y-1) {
-//         $('.header').fadeOut();
-//     } else {
-//         $('.header').fadeIn();
-//     }
-
-// });
+      canvas.onmouseup = function() {
+        pressed = false;
+      }
+      function sketch(event){
+  if (!paint) return;
+  ctx.beginPath();
+        ctx.strokeStyle = 'rgb(255, 255, 255)';
+          ctx.beginPath();
+          ctx.moveTo(coord.x, coord.y);
+          getPosition(event);
+          ctx.lineTo(coord.x , coord.y);
+          ctx.stroke();
+        }
