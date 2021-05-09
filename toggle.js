9176cb7d069a553ae60573a3fc1635b88a16f953
Anna Ketre <anna.ketre@gmail.com>
2021-05-10 00:30:58 +0100

    logofixed

--- toggle.js
+++ toggle.js
@@ -2,4 +2,10 @@ button = document.querySelector('.colortoggle');
 
 button.addEventListener('click', () => {
   document.documentElement.classList.toggle('dark-mode')
+
+  var imgs = document.getElementsByTagName("img");
+
+  for (var i = 0; i < imgs.length; i++) {
+    imgs[i].style.filter = "invert(100%)";
+  }
 });
