d0e8c0ffbeade5f10c972fd1581693ff76cc2d7e
Anna Ketre <anna.ketre@gmail.com>
2021-05-10 13:39:19 +0100

    toggle

--- toggle.js
+++ toggle.js
@@ -1,20 +1,19 @@
 var button = document.querySelector('.colortoggle');
+function change(click) {
+  if (button.style.backgroundImage == "url('public/assets/images/sun-mode.svg')") {
+    button.style.backgroundImage = "url('public/assets/images/moon-mode.svg')";
+  } else {
+    button.style.backgroundImage = "url('public/assets/images/sun-mode.svg')"
+  }
+}
 
 button.addEventListener('click', () => {
   document.documentElement.classList.toggle('dark-mode')
-
   var imgs = document.getElementsByTagName("img");
-
+  change();
   for (var i = 0; i < imgs.length; i++) {
     imgs[i].style.filter = "invert(100%)";
   }
   // button.style.backgroundImage = "url('public/assets/images/moon-mode.svg')";
 
 });
-function change(){
-if (button.style.backgroundImage == "url('public/assets/images/moon-mode.svg')") {
-  button.style.backgroundImage = "url('public/assets/images/sun-mode.svg')";
-} else {
-  button.style.backgroundImage = "url('public/assets/images/moon-mode.svg')"
-
-}}
